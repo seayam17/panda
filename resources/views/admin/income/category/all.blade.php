9bd99b2178a0065bec6e2964b79b51191ebea54b
seayam17 <seyamhridoy652@gamil.com>
2023-11-16 12:53:04 +0600

    Data Security Code Pending

--- resources/views/admin/income/category/all.blade.php
+++ resources/views/admin/income/category/all.blade.php
@@ -35,21 +35,38 @@
             <tr>
               <th>Name</th>
               <th>Remarks</th>
+              <th>Transction</th>
+              <th>Amount</th>
               <th>Manage</th>
             </tr>
           </thead>
           <tbody>
             @foreach($allData as $data)
+            @php
+            $cateID=$data->incate_id;
+            $total_income=App\Models\Income::where('income_status',1)->where('incate_id',$cateID)->sum('income_amount');
+            $income_count=App\Models\Income::where('income_status',1)->where('incate_id',$cateID)->count();
+            @endphp
             <tr>
               <td>{{$data->incate_name}}</td>
-              <td>{{$data->incate_remarks}}</td>
+              <td>{{Str::words($data->incate_remarks,3)}}</td>
+              <td>
+                @if($income_count<=9)
+                0{{$income_count}}
+                @else
+                {{$income_count}}
+                @endif
+              </td>
+              <td>{{number_format($total_income,2)}}</td>
               <td>
                 <div class="btn-group btn_group_manage" role="group">
                   <button type="button" class="btn btn-sm btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">Manage</button>
                   <ul class="dropdown-menu">
                     <li><a class="dropdown-item" href="{{url('dashboard/income/category/view/'.$data->incate_slug)}}">View</a></li>
                     <li><a class="dropdown-item" href="{{url('dashboard/income/category/edit/'.$data->incate_slug)}}">Edit</a></li>
+                    @if($income_count=='0')
                     <li><a class="dropdown-item" href="#" id="softDelete" data-bs-toggle="modal" data-bs-target="#softDeleteModal" data-id="{{$data->incate_id}}">Delete</a></li>
+                    @endif
                   </ul>
                 </div>
               </td>
@@ -73,19 +90,19 @@
   <div class="modal-dialog">
     <form method="post" action="{{url('dashboard/income/category/softdelete')}}">
       @csrf
-    <div class="modal-content modal_content">
-      <div class="modal-header modal_header">
-        <h1 class="modal-title modal_title" id="softDeleteModalLabel"><i class="fab fa-gg-circle"></i> Confirm Message</h1>
+      <div class="modal-content modal_content">
+        <div class="modal-header modal_header">
+          <h1 class="modal-title modal_title" id="softDeleteModalLabel"><i class="fab fa-gg-circle"></i> Confirm Message</h1>
+        </div>
+        <div class="modal-body modal_body">
+          Are you want to sure delete data?
+          <input type="hidden" name="modal_id" id="modal_id">
+        </div>
+        <div class="modal-footer modal_footer">
+          <button type="submit" class="btn btn-sm btn-success">Confirm</button>
+          <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal">Close</button>
+        </div>
       </div>
-      <div class="modal-body modal_body">
-         Are you want to sure delete data?
-         <input type="hidden" name="modal_id" id="modal_id">
-      </div>
-      <div class="modal-footer modal_footer">
-        <button type="submit" class="btn btn-sm btn-success">Confirm</button>
-        <button type="button" class="btn btn-sm btn-danger" data-bs-dismiss="modal">Close</button>
-      </div>
-    </div>
     </form>
   </div>
 </div>
